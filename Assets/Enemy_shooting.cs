55a540bb108e97583bfb32503a14e20ca30928c5
med1337 <leszek.bagnucki@gmail.com>
2018-09-27 16:57:23 +0100

    Tag manager update

--- Assets/Enemy_shooting.cs
+++ Assets/Enemy_shooting.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_shooting : MonoBehaviour {
-
+public class Enemy_shooting : MonoBehaviour
+{
     [SerializeField] GameObject bullet_prefab;
     [SerializeField] float vision_radius = 5.0f;
     [SerializeField] float shooting_delay = 1.0f;
 
     private float shooting_timer = 0.0f;
 
+
     // Use this for initialization
     void Start()
     {
-
     }
+
 
     // Update is called once per frame
     void Update()
@@ -22,20 +23,23 @@ public class Enemy_shooting : MonoBehaviour {
         if (!TimeManager.Instance.MovementDetected) return;
 
         shooting_timer += Time.deltaTime;
-
-        Collider2D hit_collider = Physics2D.OverlapCircle(transform.position, vision_radius);
-
-        if (hit_collider.gameObject.GetComponent<PlayerMovement>())
+        if (shooting_timer > shooting_delay)
         {
-            if (shooting_timer > shooting_delay)
+            Collider2D[] hit_collider =
+                Physics2D.OverlapCircleAll(transform.position, vision_radius);
+            foreach (var d in hit_collider)
             {
-                shooting_timer = 0.0f;
-                GameObject bullet = Instantiate(bullet_prefab, transform.position, Quaternion.identity);
-                //bullet.GetComponent<bullet_movement>().SetTarget(hit_collider.gameObject.transform.position);
+                if (d.gameObject.GetComponent<PlayerMovement>())
+                {
+                    shooting_timer = 0.0f;
+                    GameObject bullet = Instantiate(bullet_prefab, transform.position, Quaternion.identity);
+                    //bullet.GetComponent<bullet_movement>().SetTarget(hit_collider.gameObject.transform.position);
 
-                bullet.GetComponent<bullet_movement>().SetTarget(hit_collider.gameObject.transform);
+                    bullet.GetComponent<bullet_movement>().SetTarget(d.gameObject.transform);
+                    break;
+                }
+
             }
-            
         }
     }
 }
--- Assets/bullet_movement.cs
+++ Assets/bullet_movement.cs
@@ -6,22 +6,28 @@ public class bullet_movement : MonoBehaviour {
 
     
     [SerializeField] float max_speed = 3.0f;
-
+    TrailRenderer tr;
     private bool fired = false;
 
+    private float timer = 0;
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+	{
+	    tr = GetComponent<TrailRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
-    {
+	{
         if (!TimeManager.Instance.MovementDetected)
         {
+            //tr.time = Mathf.Infinity;
+            //timer = 0;
             return;
         }
-        
+
+	    //timer += Time.deltaTime;
+     //   tr.time = 1+ timer;
         if (fired)
         {
             float step = max_speed * Time.deltaTime;
