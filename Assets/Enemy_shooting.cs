8473564dbfc402fd7035337b80985db8aa0d5b83
Elliott Smith <elliottsmith1@hotmail.co.uk>
2018-10-04 18:53:48 +0100

    Updated enemy shooting
    -Will now only shoot player if can see them

--- Assets/Enemy_shooting.cs
+++ Assets/Enemy_shooting.cs
@@ -13,7 +13,7 @@ public class Enemy_shooting : MonoBehaviour
 
     [SerializeField] GUN equipped_gun = GUN.Pistol;
     [SerializeField] GameObject bullet_prefab;
-    [SerializeField] float vision_radius = 5.0f;
+    [SerializeField] int vision_radius = 5;
     [SerializeField] float shooting_delay = 1.0f;
     [SerializeField] float burst_shooting_delay = 0.4f;
 
@@ -26,23 +26,23 @@ public class Enemy_shooting : MonoBehaviour
     private float shooting_timer = 0.0f;
     private float burst_shooting_timer = 0.0f;
 
+    private GameObject player_ref;
+
 
     // Use this for initialization
     void Start()
     {
+        player_ref = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (!TimeManager.Instance.MovementDetected) return;
+        if (!TimeManager.Instance.MovementDetected) return;        
 
         shooting_timer += TimeManager.Instance.TimeScale;
         burst_shooting_timer += TimeManager.Instance.TimeScale;
 
-        //shooting_timer += Time.deltaTime;
-        //burst_shooting_timer += Time.deltaTime;
 
         if ((equipped_gun == GUN.Rifle) && (burst_shooting_timer <= burst_shooting_delay))
         {
@@ -56,11 +56,11 @@ public class Enemy_shooting : MonoBehaviour
                 burst_counter = 0;
             }
 
-            Collider2D[] hit_collider =
-                Physics2D.OverlapCircleAll(transform.position, vision_radius);
-            foreach (var d in hit_collider)
-            {
-                if (d.gameObject.GetComponent<PlayerMovement>())
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, (player_ref.transform.position - transform.position), vision_radius);
+
+            if (hit.collider != null)
+            {                
+                if (hit.collider.tag == "Player")
                 {
                     shooting_timer = 0.0f;
                     burst_shooting_timer = 0.0f;
@@ -70,7 +70,7 @@ public class Enemy_shooting : MonoBehaviour
                         for (int i = 0; i < shotgun_pellet_num; i++)
                         {
                             GameObject bullet = Instantiate(bullet_prefab, transform.position, Quaternion.identity);
-                            bullet.GetComponent<bullet_movement>().SetTarget(d.gameObject.transform, shotgun_accuracy);
+                            bullet.GetComponent<bullet_movement>().SetTarget(player_ref.gameObject.transform, shotgun_accuracy);
                         }
                     }
 
@@ -81,17 +81,26 @@ public class Enemy_shooting : MonoBehaviour
                         if (equipped_gun == GUN.Rifle)
                         {
                             burst_counter++;
-                            bullet.GetComponent<bullet_movement>().SetTarget(d.gameObject.transform, rifle_accuracy);
+                            bullet.GetComponent<bullet_movement>().SetTarget(player_ref.gameObject.transform, rifle_accuracy);
                         }
                         else
                         {
-                            bullet.GetComponent<bullet_movement>().SetTarget(d.gameObject.transform, pistol_accuracy);
+                            bullet.GetComponent<bullet_movement>().SetTarget(player_ref.gameObject.transform, pistol_accuracy);
                         }
-                        
+
                     }
-                    break;
                 }
             }
+
+            //Collider2D[] hit_collider =
+            //    Physics2D.OverlapCircleAll(transform.position, vision_radius);
+            //foreach (var d in hit_collider)
+            //{
+            //    if (d.gameObject.GetComponent<PlayerMovement>())
+            //    {
+                    
+            //    }
+            //}
         }
     }
 }
