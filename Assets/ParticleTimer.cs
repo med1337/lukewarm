d8f28ffaef5bf71fd9f97df58a2e7eb70908edf2
Elliott Smith <elliottsmith1@hotmail.co.uk>
2018-10-05 18:57:33 +0100

    Enemy death particles

---
+++ Assets/ParticleTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTimer : MonoBehaviour {
+
+    private bool playing = false;
+    private ParticleSystem ps;
+    ParticleSystem.MainModule main;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ps = GetComponent<ParticleSystem>();
+        main = ps.main;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        main.simulationSpeed = TimeManager.Instance.Scale;
+    }
+}
